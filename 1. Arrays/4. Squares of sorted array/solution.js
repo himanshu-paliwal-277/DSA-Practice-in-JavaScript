0993d5084e212cc0caa2ddee8c8f25532ac139f3
Himanshu Paliwal <hpaliwal364@gmail.com>
2024-03-20 17:56:24 +0530

    Squares of sorted array problem best solution for the problem using two pointer is added

--- 1. Arrays/4. Squares of sorted array/solution.js
+++ 1. Arrays/4. Squares of sorted array/solution.js
@@ -1,13 +1,38 @@
 // 977. Squares of sorted array
 // solution:
 
+// Using simple for loop 
+// var sortedSquares = function (nums) {
+//   for (let i = 0; i < nums.length; i++) {
+//     nums[i] = nums[i] * nums[i];
+//   }
+//   nums.sort((a, b) => a - b);
+//   return nums;
+// };
+
+// Using 2 pointer technique
 var sortedSquares = function (nums) {
-  for (let i = 0; i < nums.length; i++) {
-    nums[i] = nums[i] * nums[i];
+  let left = 0;
+  let right = nums.length - 1;
+  let arr = new Array(nums.length);
+  let index = arr.length - 1;
+  while(left <= right){
+    let Square_1 = nums[left]**2;
+    let Square_2 = nums[right]**2;
+    if(Square_1 > Square_2){
+      arr[index] = Square_1;
+      left++;
+    }
+    else {
+      arr[index] = Square_2;
+      right--;
+    }
+    index--;
   }
-  nums.sort((a, b) => a - b);
-  return nums;
+  return arr;
 };
+
+
 
 // Example 1:
 console.log(sortedSquares([-4, -1, 0, 3, 10]));
